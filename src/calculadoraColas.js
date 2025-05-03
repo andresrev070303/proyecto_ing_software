8369e43ff93914b2f456a50600d645171db8d56f
andresrev070303 <andres.revollo@ucb.edu.bo>
2025-05-03 12:50:26 -0400

    feat: deberia devolver la capacidad de carga basdo en la cantidad de litros

--- src/calculadoraColas.js
+++ src/calculadoraColas.js
@@ -1,3 +1,6 @@
 export function calcularVehiculosEnCola(distancia) {
     return Math.round ( distancia / 6);
   }
+  export function calcularCapacidadCarga(combustibleTotal) {
+    return Math.round(combustibleTotal / 50);
+  }
--- src/calculadoraColas.test.js
+++ src/calculadoraColas.test.js
@@ -1,10 +1,13 @@
-import { calcularVehiculosEnCola } from "./calculadoraColas.js";
+import { calcularVehiculosEnCola, calcularCapacidadCarga } from "./calculadoraColas.js";
 
-describe("calcularVehiculosEnCola", () => {
+describe("calcularCola", () => {
   it("debería devolver 2 para una distancia de 12", () => {
     expect(calcularVehiculosEnCola(12)).toBe(2);
   });
   it("debería devolver 4 para una distancia de 21", () => {
     expect(calcularVehiculosEnCola(21)).toBe(4); // 3.5 redondea a 4
+  });
+  it("debería devolver 2 para 100 litros", () => {
+    expect(calcularCapacidadCarga(100)).toBe(2);
   });
 });
